7cfe21771524b47316c7c0f2cfe604dc49c4088d
Sthefany Albuquerque <s.albuquerque@aluno.ifsp.edu.br>
2020-01-16 17:35:58 -0300

    Update Regular Expressions I.js

--- Day 7 - Dia 7/Regular Expressions I - Expressões regulares I/Regular Expressions I.js
+++ Day 7 - Dia 7/Regular Expressions I - Expressões regulares I/Regular Expressions I.js
@@ -1,13 +1,13 @@
 function regexVar() {
-     / *
+     /*
      * Declare uma variável de objeto RegExp denominada 're'
      * Ele deve corresponder a uma sequência que começa e termina com a mesma vogal (por exemplo, {a, e, i, o, u})
-     * /
+     */
     
     var re = RegExp(/^([aeiou]).*\1$/);
     
-     / *
+     /*
      * Não remova a declaração de devolução
-     * /
+     */
     return re;
 }
