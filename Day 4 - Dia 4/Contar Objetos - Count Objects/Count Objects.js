8c7e844b84f72dcdd69dff29794e62ee49cff9e8
Sthefany Albuquerque <s.albuquerque@aluno.ifsp.edu.br>
2020-02-11 16:02:13 -0300

    Update Count Objects.js

--- Day 4 - Dia 4/Contar Objetos - Count Objects/Count Objects.js
+++ Day 4 - Dia 4/Contar Objetos - Count Objects/Count Objects.js
@@ -1,9 +1,9 @@
 /*
- * Return a count of the total number of objects 'o' satisfying o.x == o.y.
- * 
- * Parameter(s):
- * objects: an array of objects with integer properties 'x' and 'y'
- */
+  * Retorne uma contagem do número total de objetos 'o' satisfazendo o.x == o.y.
+  *
+  * Parâmetro (s):
+  * objetos: uma matriz de objetos com propriedades inteiras 'x' e 'y'
+ */
 function getCount(objects) {
     var count = 0;
     for (var index in objects)
